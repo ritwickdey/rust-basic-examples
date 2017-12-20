e1e64005219591997dae87675e91df2c45bfb384
Prithwee Das <prithweedas123@gmail.com>
2017-12-21 00:12:01 +0530

    github page completed

---
+++ docs/scripts/scripts.js
@@ -0,0 +1,23 @@
+const github = document.querySelector(".github");
+
+github.addEventListener("click", () => {
+  window.open("https://github.com/prithweedas", "_blank");
+});
+
+const repo = document.querySelector(".repo");
+
+repo.addEventListener("click", () => {
+  window.open("https://github.com/prithweedas/rust-basic-examples", "_self");
+});
+
+const twitter = document.querySelector(".twitter");
+
+twitter.addEventListener("click", () => {
+  window.open("https://twitter.com/prithweedas");
+});
+
+const facebook = document.querySelector(".facebook");
+
+facebook.addEventListener("click", () => {
+  window.open("https://www.facebook.com/prithweedas");
+});
